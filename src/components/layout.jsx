102773332804f8404697ac1a79393729569c6c4c
CudaRabbani <md.reza.rabbani@gmail.com>
2019-08-08 15:51:21 -0600

    Initial Layout. Needs to be changed later.

---
+++ src/components/layout.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Header from "./header";
+
+class Layout extends Component {
+    render() {
+        return (
+            <div className="container full-layout">
+                <div className="row">
+                    <div className="col">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="container m-1">
+                    <div className="row">
+                        <div className="col left-panel m-1">Sensor Drag and Drop List</div>
+                        <div className="col workspace-panel m-1">Workspace</div>
+                        <div className="col right-panel m-1">Output</div>
+                    </div>
+                    <div className="container footer m-1">Footer</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Layout;
